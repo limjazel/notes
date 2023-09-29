ae73bd326d9c5afead0d784ef0018145a5a96813
Jazel Lim <lim.jazel@gmail.com>
2023-09-29 18:18:08 +0800

    Update config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress"
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+	base: "/notes",
 	title: "Notes",
 	description: "Wiki, docs, notes and ramblings.",
 	themeConfig: {
